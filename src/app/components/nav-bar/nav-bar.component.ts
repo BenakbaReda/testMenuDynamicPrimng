ead1e0a0b63674a88dbae4f538bc967f71695ac5
BenakbaReda <75283738+BenakbaReda@users.noreply.github.com>
2022-09-13 23:20:37 +0100

    Add files via upload

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -41,154 +41,45 @@ export class NavBarComponent implements OnInit {
 
   buildlistmenu( List: Icategorie[])  
  {
-    let items : IMenuCategorie[]=[];
+ 
     let item : IMenuCategorie;
-    const listIdParent : number[]=[];
-
-    List.forEach(x =>
-    {
-        let idparent = listIdParent.find(nbr => nbr== x.idParent)
-        if((idparent == null) || (idparent == undefined)  ){
-            listIdParent.push(x.idParent);
-        }
-    })
-    listIdParent.sort();
-    console.log(listIdParent)
+    item ={ id : 0, idroot:0, name: "categorie"};
+    item.items = this.buildMenuCategorie(List);
+    console.log( item);
 
 
-    listIdParent.forEach(x =>
-    {
-        item ={ id : x, idroot:0, name: ""};
-        item.items = this.getchildofParent(List,x );
-        items.push(item);
-    })
+
+
+
+ }
+
+
  
-    console.log( items);
-
- }
-
-
-getchildofParent( List: Icategorie[], parentid:number=0, root : IMenuCategorie =null ) : IMenuCategorie[]
- {
-    let items : IMenuCategorie[];
-    let elm : IMenuCategorie
-
-    List.forEach(x =>
-    {
-        
-        if ( x.idParent ==parentid){
-               
-            if(parentid ==0){
-                if(items ==null) items =[];
-                elm ={ id : x.id,idroot:x.idParent , name: x.displayname};
-                items.push(elm);
-            }
-            else
-            {
-                if(root !=null)
-                {
-                    let exist = root.items.find(y=> y.id== parentid)
-                    exist.items=this.getchildofParent(List, exist.id,  exist)
-                }
-
-
-            }
-
-        }
-        
-        
-       
-    })
-    return items;
- }
-
-
- getchildofParentold( List: Icategorie[], parentid:number=0, item: IMenuCategorie) : IMenuCategorie[]
- {
-    let items : IMenuCategorie[]
- 
-
-    List.forEach(x =>
-    {
-        
-        if ( x.idParent ==parentid){
-   
-            if(items ==null) items =[];
-           // items.push( { id : x.id, name: x.displayname});
-        }
-       
-    })
-    return items;
- }
-
-
-
-buildMenu( List: Icategorie[] )
+ buildMenuCategorie( List: Icategorie[],   parentid:number=0) : IMenuCategorie[]
 {
-    const listIdParent : number[]=[];
-    List.forEach(x =>
-    {
-        let idparent = listIdParent.find(nbr => nbr== x.idParent)
-        if((idparent == null) || (idparent == undefined)  ){
-            listIdParent.push(x.idParent);
-        }
-    })
-
-    console.log( "listIdParent : " + JSON.stringify(listIdParent));
-    let MenuCategorie:IMenuCategorie[];
-
-    listIdParent.forEach(x =>
-    {
-        let childItem :  MenuItem[]  = this.findChildOfParent(List, x);
-        childItem.forEach(elm =>
-        {
-            console.log( " [ ID : " + elm.id );
-        })
-
-
-    })
-
-
-
-}
- 
-
-
-
- 
-
-findChildOfParent( List: Icategorie[] ,  idParent:number =0): MenuItem[]
-{
-    let parentItem :  MenuItem  ;
-    let childItem :  MenuItem[] =null;
-    List.forEach(x =>
-    {
-       // console.log( "Icategorie : " + JSON.stringify(x));
-       // console.log( "Icategorie  [ ID : " + x.id + ", Parent : " + x.idParent + "]");
-       // parentItem = childItem.find(elm  => elm.id == "Root" + x.idParent + "_id" + x.id  )
-        if ( x.idParent ==idParent){
-                if(childItem ==null) childItem =[];
-                parentItem = {
-                    id :"Parent" + x.idParent + "_child" + x.id ,
-                    label: x.displayname,
-                    icon:'pi pi-fw pi-file' 
-                }
-                childItem.push(parentItem);
-               //  parentItem.items= this.findChildOfParent(this.Categories,x.idParent )  
-               //  console.log( "item  : ID : " +  parentItem.id );
+  const items : IMenuCategorie[]=[];
+ // console.log( "buildmenull  parentid :" +parentid  );
+   List.forEach(cat =>
+   {
+       if ( cat.idParent == parentid ){
+            const elm = { id : cat.id, idroot: cat.idParent , name: cat.displayname};
+            items.push(elm);
+            //console.log( "add elm  Id :" + cat.id  );
         }
         else
         {
-
-
-
+            const rootitem = items.find(y=> y.id== cat.idParent)
+             if ( rootitem ){
+               // console.log( "found item  ID :" +rootitem.id  );
+       
+                rootitem.items=this.buildMenuCategorie(List, cat.idParent) 
+            }
         }
-    })
-  //  console.log( "childItem : " + JSON.stringify(childItem));
-    return childItem;
+   }) 
 
+   // console.log( "Icategorie : " + JSON.stringify(x));
+   return items;
 }
 
  
-
 }
